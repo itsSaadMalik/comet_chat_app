8bd58ab296cf33e3a5b3d08277ad428a344a7f3f
saad <saadmalikin@gmail.com>
2025-07-25 18:46:31 +0530

    more and more done with login screen

---
+++ lib/features/new chat/presentation/screens/new_chat_screen.dart
@@ -0,0 +1,12 @@
+import 'package:cometchat_calls_uikit/cometchat_calls_uikit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class NewChatScreen extends ConsumerWidget {
+  const NewChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
